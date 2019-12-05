86b29bb537426089dcb7546ffe302eb6acd81afb
adam h <ajhanson22227@gmail.com>
2019-12-05 14:45:19 -0600

    Added feature to search youtube and play first result

--- here we go again/commands/play.js
+++ here we go again/commands/play.js
@@ -1,11 +1,14 @@
 const ytdl = require('ytdl-core');
 const queue = new Map();
+const Youtube = require('simple-youtube-api');
+const { youtubeAPI } = require('../config.json');
+const youtube = new Youtube(youtubeAPI);
 
 module.exports = {
 
     name: 'play',
     description: 'Plays the given youtube link',
-    execute(client, message, args){
+    async execute(client, message, args){
         message.delete()
         const serverQueue = queue.get(message.guild.id);
 
@@ -19,13 +22,21 @@ module.exports = {
         }
         let validate = ytdl.validateURL(args[0])
         if (!validate){
-            message.channel.send("Please enter a valid YouTube link")
-            return
+            //message.channel.send("Please enter a valid YouTube link")
+            //return
+            const videoString = await youtube.searchVideos(args, 1);
+            var video = await youtube.getVideoByID(videoString[0].id);
+            message.channel.send(`Now Playing ${video.title}`);
+            message.channel.send(video.thumbnails.high.url);
+            var songurl = `https://www.youtube.com/watch?v=${video.raw.id}`;
+
         }
-        var song = args[0]
+        else{
+            var songurl = args[0]
+        }
 
         message.member.voiceChannel.join().then(connection => {
-            const dispatcher = connection.playStream(ytdl(song))
+            const dispatcher = connection.playStream(ytdl(songurl))
             dispatcher.setVolume(.1)
 
             dispatcher.on("error", error => {
