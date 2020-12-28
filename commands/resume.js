98b560b7946111cbe43e9dd4930663cff9a45878
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-28 13:54:12 -0600

    Change voiceConnection to current voice.connection + Prettier

--- commands/resume.js
+++ commands/resume.js
@@ -1,22 +1,18 @@
 module.exports = {
-
-    name: 'resume',
-    description: 'resume audio',
-    execute(client, message, args){
-
-    	try{
-    		message.delete(0)
-    	}
-    	catch(e){
-    		message.channel.send("Woops, can't delete message")
-    	}
-        
-        try{
-	        connection = message.guild.voiceConnection;
-	        connection.dispatcher.resume();
-	    }
-	    catch(e){
-	    	return message.channel.send("Uh-Oh. Error. Am I not in a Channel?");
-	    }
+  name: "resume",
+  description: "resume audio",
+  execute(client, message, args) {
+    try {
+      message.delete(0);
+    } catch (e) {
+      message.channel.send("Woops, can't delete message");
     }
-}
+
+    try {
+      connection = message.guild.voice.connection;
+      connection.dispatcher.resume();
+    } catch (e) {
+      return message.channel.send("Uh-Oh. Error. Am I not in a Channel?");
+    }
+  },
+};
