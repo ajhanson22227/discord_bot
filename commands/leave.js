594fbba8aef5e1dae306dc113c5eb4821546336c
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-28 13:31:35 -0600

    Change voiceChannel to the current voice.channel + Prettier

--- commands/leave.js
+++ commands/leave.js
@@ -1,27 +1,24 @@
 module.exports = {
-
-    name: 'leave',
-    description: 'Leaves the voice channel',
-    execute(client, message, args){
-
-        try{
-            message.delete(0)
-        }
-        catch(e){
-            message.channel.send("Woops Can't Delete Message");
-        }
-
-        try{
-        	if (!message.member.voiceChannel){
-                    return message.channel.send("You're not in my channel. You're not the boss of me!")
-                }
-                else{
-                	message.member.voiceChannel.leave()
-                	return
-                }
-            }
-        catch(e){
-        	return message.channel.send("UwU I made a mistake. Sowwy");
-            }
+  name: "leave",
+  description: "Leaves the voice channel",
+  execute(client, message, args) {
+    try {
+      message.delete(0);
+    } catch (e) {
+      message.channel.send("Woops Can't Delete Message");
     }
-}
+
+    try {
+      if (!message.member.voice.channel) {
+        return message.channel.send(
+          "You're not in my channel. You're not the boss of me!"
+        );
+      } else {
+        message.member.voice.channel.leave();
+        return;
+      }
+    } catch (e) {
+      return message.channel.send("UwU I made a mistake. Sowwy");
+    }
+  },
+};
