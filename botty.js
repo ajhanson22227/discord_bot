d1884ec504c83df0271625e23bffd27ca52b2acd
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-28 11:54:49 -0600

    Get API from env file + Prettier

--- botty.js
+++ botty.js
@@ -1,42 +1,43 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token} = require('./config.json');
+const fs = require("fs");
+const Discord = require("discord.js");
+
+require("dotenv").config();
+const token = process.env.token;
+const prefix = "!";
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-
-
-
-client.once('ready', ()=>{
-    console.log('Ready Freddy!');
+client.once("ready", () => {
+  console.log("Ready Freddy!");
 });
 
-client.on('message', message =>{
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    if (!client.commands.has(command)){
-        message.channel.send(`'${command}' is not a command`)
-        return
-    }
+  if (!client.commands.has(command)) {
+    message.channel.send(`'${command}' is not a command`);
+    return;
+  }
 
-    try{
-        client.commands.get(command).execute(client, message, args)
-    }catch(error){
-        console.error(error);
-        message.reply('There was an error running that command')
-    }
+  try {
+    client.commands.get(command).execute(client, message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("There was an error running that command");
+  }
 });
 
-
-client.login(token)
+client.login(token);
