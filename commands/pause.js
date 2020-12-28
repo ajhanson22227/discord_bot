50b2a328d5c11d69f0b87cbf15ab74cceee3a31f
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-28 13:53:40 -0600

    Change voiceConnection to current voice.connection + Prettier

--- commands/pause.js
+++ commands/pause.js
@@ -1,23 +1,18 @@
 module.exports = {
-
-    name: 'pause',
-    description: 'Pause audio',
-    execute(client, message, args){
-    	
-    	try{
-    		message.delete(0)
-    	}
-    	catch(e){
-    		message.channel.send("Woops Can't Delete Message");
-    	}
-
-
-    	try{
-	        connection = message.guild.voiceConnection;
-	        connection.dispatcher.pause();
-	    }
-	    catch(e){
-	    	return message.channel.send("Uh-Oh. Error. Am I not in a Channel?");
-	    }
+  name: "pause",
+  description: "Pause audio",
+  execute(client, message, args) {
+    try {
+      message.delete(0);
+    } catch (e) {
+      message.channel.send("Woops Can't Delete Message");
     }
-}
+
+    try {
+      connection = message.guild.voice.connection;
+      connection.dispatcher.pause();
+    } catch (e) {
+      return message.channel.send("Uh-Oh. Error. Am I not in a Channel?");
+    }
+  },
+};
