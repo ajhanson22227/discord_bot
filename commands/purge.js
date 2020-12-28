072ad19965f5275440e488b71cf958503b0b5623
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-28 12:09:18 -0600

    Remove bot msg.delete + Prettier

--- commands/purge.js
+++ commands/purge.js
@@ -1,21 +1,18 @@
 module.exports = {
+  name: "purge",
+  description: "Deletes up to 100 Messages",
+  execute(client, message, args) {
+    deleteNumber = Number(args[0]);
 
-    name: 'purge',
-    description: 'Deletes up to 100 Messages',
-    execute(client, message, args){
-        deleteNumber = Number(args[0]);
-
-        if (deleteNumber <= 0){
-            message.channel.send("Use a number 1-99");
-            return;
-        }
-        try{
-            message.channel.bulkDelete(deleteNumber).then(() =>{
-                message.channel.send(`Deleted ${deleteNumber} messages`).then(msg => msg.delete(3000));
-                });
-            }catch(error){
-                console.error(error)
-                message.channel.send("There was a problem running")
-            }
+    if (deleteNumber <= 0 || deleteNumber > 99) {
+      message.channel.send("Use a number 1-99");
+      return;
     }
-}
+    try {
+      message.channel.bulkDelete(deleteNumber + 1);
+    } catch (error) {
+      console.error(error);
+      return message.channel.send("There was a problem running");
+    }
+  },
+};
